bd40cbd28930d602359b63d3e9ce2cb70098b473
girvel <widauka@ya.ru>
2024-12-02 16:04:19 +0500

    My own personal hello world

--- via_raylib/main.c
+++ via_raylib/main.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(800, 450, "Game of Life");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Hello world!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
