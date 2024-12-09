1e98a3232a1984b04734d1dda462de8834fd2399
girvel <widauka@ya.ru>
2024-12-09 23:13:25 +0500

    Make the game space a torus

--- via_love/shader.glsl
+++ via_love/shader.glsl
@@ -1,6 +1,7 @@
 vec2 right = vec2(1 / love_ScreenSize.x, 0.);
 vec2 down = vec2(0., 1 / love_ScreenSize.y);
 
+// TODO try 2 CELL states with different colours
 vec4 CELL = vec4(1.);
 vec4 NO_CELL = vec4(0., 0., 0., 1.);
 
@@ -8,14 +9,14 @@ vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
     // TODO %
     // TODO does neighbours_n being float affect performance?
     float neighbours_n = 
-        Texel(tex, texture_coords + right).x +
-        Texel(tex, texture_coords - right).x +
-        Texel(tex, texture_coords + down).x +
-        Texel(tex, texture_coords - down).x +
-        Texel(tex, texture_coords + right + down).x +
-        Texel(tex, texture_coords - right + down).x +
-        Texel(tex, texture_coords + right - down).x +
-        Texel(tex, texture_coords - right - down).x;
+        Texel(tex, mod(texture_coords + right, 1.)).x +
+        Texel(tex, mod(texture_coords - right, 1.)).x +
+        Texel(tex, mod(texture_coords + down, 1.)).x +
+        Texel(tex, mod(texture_coords - down, 1.)).x +
+        Texel(tex, mod(texture_coords + right + down, 1.)).x +
+        Texel(tex, mod(texture_coords - right + down, 1.)).x +
+        Texel(tex, mod(texture_coords + right - down, 1.)).x +
+        Texel(tex, mod(texture_coords - right - down, 1.)).x;
 
     vec4 it = Texel(tex, texture_coords);
 
